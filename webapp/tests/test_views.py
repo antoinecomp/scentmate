29de614b36dec8740435316b17cbaff791a17dae
Mangouste <antoine.compagnie@gmail.com>
2022-02-12 15:37:58 +0100

    create test for views

--- webapp/tests/test_views.py
+++ webapp/tests/test_views.py
@@ -1,12 +1,7 @@
-import unittest
-
 from django.test import TestCase
-from .todo.models import Keyword
+from todo.models import Keyword
+from django.contrib.auth.models import User # Required to assign User as a borrower
 
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
 
 # Je veux tester que lorsque je fais une rerquête elle aboutie bien.
 class QuizzTestCase(TestCase):
@@ -16,9 +11,47 @@ class QuizzTestCase(TestCase):
             # Create keywords for pagination tests
             keywords = ["Fruity"]
 
+
+class HomeViewTest(TestCase):
     def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('/quiz/getmatch/')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+
+class SimilarViewTest(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/similar/')
+        self.assertEqual(response.status_code, 200)
+
+
+class LoginViewTest(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+
+
+class ProfileViewTest(TestCase):
+    def setUp(self):
+        # Create two users
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+
+        test_user1.save()
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('profile'))
+        self.assertRedirects(response, '/login/')
+
+    def test_logged_in_uses_correct_template(self):
+        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        response = self.client.get(reverse('profile'))
+
+        # Check our user is logged in
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        # Check that we got a response "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check we used correct template
+        self.assertTemplateUsed(response, 'templates/todo/loginuser.html')
 
 
 if __name__ == '__main__':
